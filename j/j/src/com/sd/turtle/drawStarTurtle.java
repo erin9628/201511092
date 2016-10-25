a4bdbc4aa310594f9a0b8c960ac3660782c6ab99
erin9628 <erin9628@naver.com>
2016-10-26 04:31:52 +0900

    Add files via upload

---
+++ j/j/src/com/sd/turtle/drawStarTurtle.java
@@ -0,0 +1,14 @@
+package com.com.turtle;
+import ch.aplu.turtle.*;
+
+class drawStarTurtle {
+  public static void main(String [] args) {
+  
+  Turtle t1 = new Turtle();
+  
+  for (int i=0;i<5;i++) {
+    t1.forward(100);
+    t1.right(144);
+    }
+  }
+}
